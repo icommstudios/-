31fe2aed63bb77cb1de8702d9afaded03afe5696
Travis Hedrick <iCommStudios@Traviss-MacBook-Pro.local>
2013-11-02 14:27:30 -0400

    Daily Commit

--- wp-content/themes/AFS-theme/templates/content-home.php
+++ wp-content/themes/AFS-theme/templates/content-home.php
@@ -1,5 +1,7 @@
-<p>home page</p>
-<div class="container">
+<div class="img-hero img-hero-bar">
+	<h1>Welcome to Alliance Facility Solutions, where we make it easy for facility owners and managers to find qualified, licensed contractors.</h1>
+</div>
+<div class="container-overflow">
 <?php while (have_posts()) : the_post(); ?>
   <?php the_content(); ?>
   <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
