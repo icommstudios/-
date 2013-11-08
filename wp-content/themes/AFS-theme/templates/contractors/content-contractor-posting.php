7067d15092bdff1732799b49e5add84b1fab03d5
StephenSmall Design <stephen@stephen-small.com>
2013-11-08 14:05:50 -0500

    Changes to contractor posting
    Adding new content per the PSD

--- wp-content/themes/AFS-theme/templates/contractors/content-contractor-posting.php
+++ wp-content/themes/AFS-theme/templates/contractors/content-contractor-posting.php
@@ -1,1 +1,43 @@
-<p>Manage this in templates/contractors/content-contractor-posting.php</p>
+<div class="main-content">
+<ul class="itinerary">
+	<li>Est. 2002</li>
+	<li><i class="fa fa-envelope-o"></i> email@email.com</li>
+	<li><i class="fa fa-phone"></i> 704-555-5555</li>	
+	<li><i class="fa fa-clock-o"></i> 9am - 5pm est.</li>	
+</ul>
+<div class="featured-img">
+	<img src="http://placehold.it/870x294">
+</div>
+<p>Bacon ipsum dolor sit amet flank andouille pancetta venison swine, drumstick pig tongue brisket. Pastrami boudin bacon pork chop swine beef ribs. Tongue flank corned beef venison, meatball biltong shank short ribs ribeye. Turkey tenderloin ham chuck. Shank beef venison filet mignon hamburger sirloin swine kielbasa turkey sausage ribeye bresaola. Pork chop pastrami pork, turkey spare ribs doner tri-tip jowl kielbasa swine tongue prosciutto biltong.</p>
+<p>Pastrami boudin bacon pork chop swine beef ribs. Tongue flank corned beef venison, meatball biltong shank short ribs ribeye. Turkey tenderloin ham chuck. Shank beef venison filet mignon hamburger sirloin swine kielbasa turkey sausage ribeye bresaola. Pork chop pastrami pork, turkey spare ribs doner tri-tip jowl kielbasa swine tongue prosciutto biltong</p>
+<ul class="nav nav-tabs" id="myTab">
+  <li class="active"><a href="#general" data-toggle="tab">General</a></li>
+  <li><a href="#licenses" data-toggle="tab">Licenses & Accreditations</a></li>
+  <li><a href="#ratings" data-toggle="tab">Ratings & Referrals</a></li>
+</ul>
+
+<div class="tab-content">
+  <div class="tab-pane active" id="general">
+  	<ul>
+  		<li>John C. Smith</li>
+  		<li>XYZ Company</li>
+  		<li>704-555-5555</li>
+  		<li>email@email.com</li>
+  		<li>Charlotte, NC</li>
+  	</ul>
+  	<ul>
+  		<li>9am - 5pm est.</li>
+  		<li>15 yrs. experience</li>
+  		<li>www.yourwebsite.com</li>
+  	</ul>
+  	<ul class="skillsets">
+  		<p>core skills</p>
+  		<li>Construction</li>
+  		<li>Consturction</li>
+  	</ul>
+  </div>
+  <div class="tab-pane" id="licenses">...</div>
+  <div class="tab-pane" id="ratings">...</div>
+</div>
+</div>
+
