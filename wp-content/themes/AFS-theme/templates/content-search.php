055ef2c53fe642affab594020859d250ea52f16b
Travis Hedrick <iCommStudios@Traviss-MacBook-Pro.local>
2013-11-09 13:07:21 -0500

    search page template

--- wp-content/themes/AFS-theme/templates/content-search.php
+++ wp-content/themes/AFS-theme/templates/content-search.php
@@ -18,10 +18,26 @@
 
 <hr />
 
-<article class="posting job">
+<article class="posting job-post">
   	<time>10/09/13</time>
     <h2 class="posting-title"><a href="#">Post Title</a></h2>
     <p class="posting-summary">If you have ever experienced the thrill of setting up an email app or client, you most certainly have had to deal with the terms POP and IMAP. However, you might not be able to recall which protocol you chose, … <a href="http://icommstudios.com/web-design-blog/web-design-resources/pop-vs-imap-which-one-is-right-for-you/">Continued</a></p>
+    <div class="posting-tags">
+  		<span class="label label-primary">Construction</span>
+  		<span class="label label-primary label-location">Charlotte, NC</span>
+  </div>
+  <div class="posting-meta">
+  	<p class="author">posted by <a href="http://icommstudios.com/web-design-blog/author/thedrick/" rel="author" class="fn">thedrick</a></p>
+  </div>
+</article>
+<article class="posting contractor-post">
+  	<time>10/09/13</time>
+    <h2 class="posting-title"><a href="#">Post Title</a></h2>
+    <p class="posting-summary">If you have ever experienced the thrill of setting up an email app or client, you most certainly have had to deal with the terms POP and IMAP. However, you might not be able to recall which protocol you chose, … <a href="http://icommstudios.com/web-design-blog/web-design-resources/pop-vs-imap-which-one-is-right-for-you/">Continued</a></p>
+  <div class="posting-tags">
+  	<span class="label label-primary">Construction</span>
+  	<span class="label label-primary label-location">Charlotte, NC</span>
+  </div>
   <div class="posting-meta">
   	<p class="author">posted by <a href="http://icommstudios.com/web-design-blog/author/thedrick/" rel="author" class="fn">thedrick</a></p>
   </div>
